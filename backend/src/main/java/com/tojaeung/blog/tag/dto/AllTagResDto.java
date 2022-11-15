64de7e8b46569628e01470451308e45812f1a738
Tojaeung <kimwoon11@naver.com>
2022-11-16 01:22:22 +0900

    Feat:(be) 태그모음 api 추가

---
+++ backend/src/main/java/com/tojaeung/blog/tag/dto/AllTagResDto.java
@@ -0,0 +1,16 @@
+package com.tojaeung.blog.tag.dto;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class AllTagResDto {
+    private UUID id;
+    private String tagName;
+
+    public AllTagResDto(UUID id, String tagName) {
+        this.id = id;
+        this.tagName = tagName;
+    }
+}
