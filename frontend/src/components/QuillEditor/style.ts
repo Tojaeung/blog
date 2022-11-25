28ee64eaac32f66381981a4a8a1b25e6890b641d
Tojaeung <kimwoon11@naver.com>
2022-11-25 23:20:37 +0900

    Refactor:(fe) 에디터 폰트크기 변경

--- frontend/src/components/QuillEditor/style.ts
+++ frontend/src/components/QuillEditor/style.ts
@@ -3,31 +3,35 @@ import ReactQuill from 'react-quill';
 
 export const Editor = styled(ReactQuill)`
   width: 100%;
+  height: 700px;
   line-height: 20px;
   word-wrap: break-word;
 
   h1 {
+    font-size: 26px;
     font-weight: bold;
     font-family: ${({ theme }) => theme.font.text};
   }
   h2 {
+    font-size: 19.5px;
     font-weight: bold;
     font-family: ${({ theme }) => theme.font.text};
   }
   h3 {
+    font-size: 15.21px;
     font-weight: bold;
     font-family: ${({ theme }) => theme.font.text};
   }
   h4 {
-    font-weight: bold;
+    font-size: 13px;
     font-family: ${({ theme }) => theme.font.text};
   }
   h5 {
-    font-weight: bold;
+    font-size: 10.79px;
     font-family: ${({ theme }) => theme.font.text};
   }
   h6 {
-    font-weight: bold;
+    font-size: 10px;
     font-family: ${({ theme }) => theme.font.text};
   }
 
@@ -53,6 +57,7 @@ export const Editor = styled(ReactQuill)`
   }
 
   blockquote {
+    font-size: 14px;
     font-family: ${({ theme }) => theme.font.text};
     font-style: italic;
     font-weight: bold;
