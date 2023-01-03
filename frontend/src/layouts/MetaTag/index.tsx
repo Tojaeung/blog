aa8f6b5b96f5f5c09fe25c56c3717f80a92290ec
Tojaeung <kimwoon11@naver.com>
2023-01-04 00:32:23 +0900

    Refactor: 메타태그 description 글자 수 조정

--- frontend/src/layouts/MetaTag/index.tsx
+++ frontend/src/layouts/MetaTag/index.tsx
@@ -6,19 +6,19 @@ function MetaTag({ title, desc, keywords, image, url }: IProps) {
     <Helmet>
       <title>{title}</title>
 
-      <meta name='description' content={desc} />
+      <meta name='description' content={desc.substring(0, 100)} />
       <meta name='keywords' content={keywords?.map((keyword) => keyword.tagName).toString()} />
 
       <meta property='og:type' content='website' />
       <meta property='og:title' content={title} />
       <meta property='og:site_name' content={title} />
-      <meta property='og:description' content={desc} />
+      <meta property='og:description' content={desc.substring(0, 100)} />
       <meta property='og:image' content={image} />
       <meta property='og:url' content={url} />
 
       <meta property='twitter:card' content='summary' />
       <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={desc} />
+      <meta name='twitter:description' content={desc.substring(0, 100)} />
       <meta name='twitter:image' content={image} />
 
       <link rel='canonical' href={url} />
