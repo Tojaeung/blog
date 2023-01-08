1a9590871ec8be94bf8cbd015bddebfb1bc7cb6a
Tojaeung <kimwoon11@naver.com>
2023-01-08 22:44:34 +0900

    Fixed: 메타태그 이미지 문제

--- frontend/src/layouts/MetaTag/index.tsx
+++ frontend/src/layouts/MetaTag/index.tsx
@@ -13,13 +13,16 @@ function MetaTag({ title, desc, keywords, url }: IProps) {
       <meta property='og:title' content={title} />
       <meta property='og:site_name' content={title} />
       <meta property='og:description' content={desc.substring(0, 100)} />
-      <meta property='og:image' content='/images/profile.jpg' />
+      <meta property='og:image' content='https://tojaeung-blog-images.s3.ap-northeast-2.amazonaws.com/2021-02-27.jpg' />
       <meta property='og:url' content={url} />
 
       <meta property='twitter:card' content='summary' />
       <meta name='twitter:title' content={title} />
       <meta name='twitter:description' content={desc.substring(0, 100)} />
-      <meta name='twitter:image' content='/images/profile.jpg' />
+      <meta
+        name='twitter:image'
+        content='https://tojaeung-blog-images.s3.ap-northeast-2.amazonaws.com/2021-02-27.jpg'
+      />
 
       <link rel='canonical' href={url} />
     </Helmet>
